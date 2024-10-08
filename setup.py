387e30f008240c7ea77fb286368801e487036d62
Yishai Shor <167309853+Yishai-Shor@users.noreply.github.com>
2024-10-08 12:40:16 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="python_scroll", 
+    name="Identify Number of Writers", 
     version="0.1.0", 
     packages=find_packages(),
     install_requires=[
