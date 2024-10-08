0521cf8085e98d7d5265f9f2d4ebdcad33c301b1
Yishai Shor <167309853+Yishai-Shor@users.noreply.github.com>
2024-10-08 13:04:49 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="IdentifyWriters", 
+    name="Using-Statistical-Methods-to-Identify-Number-of-Writers-in-the-Dead-Sea-Scrolls", 
     version="0.1.0", 
     packages=find_packages(),
     install_requires=[
