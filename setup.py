cefbaf0b1202277d2323a051146995fb852336d5
Yishai Shor <167309853+Yishai-Shor@users.noreply.github.com>
2024-10-09 12:34:31 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,11 +16,10 @@ setup(
     ],
     author="Yishai Shor & Dr. Barak Sober",  
     author_email="yishai.shor@gmail.com",  
-    description="A versatile package for handling text documents and performing various statistical tests to identify the number of writers who contributed to the document",  # Brief description of the package
+    description="A versatile package for handling text documents and performing various statistical tests to identify the number of writers who contributed to the document",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/Yishai-Shor/Using-Statistical-Methods-to-Identify-Number-of-Writers-in-the-Dead-Sea-Scrolls",  
-    packages=['IdentWriters'], 
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
